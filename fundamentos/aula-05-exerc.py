ac5b66c4a1a8081025ba92ff49220b167a5c10e5
AugustoTI <augustocesar4001@gmail.com>
2023-12-01 16:22:42 -0300

    exercicio - média

---
+++ fundamentos/aula-05-exerc.py
@@ -0,0 +1,8 @@
+# Escreva um programa onde o usuário digite duas notas e ele mostre a média das duas notas
+
+nota_1 = float(input('Digite a nota da primeira avaliação: '))
+nota_2 = float(input('Digite a nota da segunda avaliação: '))
+
+media = (nota_1 + nota_2) / 2
+
+print(f'O resultado da média é: {media}')
