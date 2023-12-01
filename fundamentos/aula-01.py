df7483f1a726e5eb46e265ac4a21e0570a247d54
AugustoTI <augustocesar4001@gmail.com>
2023-11-30 23:51:43 -0300

    variaveis e tipos de dados

---
+++ fundamentos/aula-01.py
@@ -0,0 +1,11 @@
+# Variaveis e tipos de dados
+
+# -> Receber dados
+# -> Armazenar dados
+# -> Processar dados
+# -> Mostrar dados
+
+year = 2023 # Int
+price = 9.99 # Float
+name = 'Augusto César' # String
+isMan = True # Boolean
