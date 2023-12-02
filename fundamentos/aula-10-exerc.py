f91740b087081d9596b250b2951997e9af0b7a45
AugustoTI <augustocesar4001@gmail.com>
2023-12-02 14:24:00 -0300

    exercicio - maior número

---
+++ fundamentos/aula-10-exerc.py
@@ -0,0 +1,12 @@
+# Mostre o maior número dentre 3 numeros recebidos
+
+numero_1 = int(input('Digite o primeiro número: '))
+numero_2 = int(input('Digite o segundo número: '))
+numero_3 = int(input('Digite o terceiro número: '))
+
+if(numero_1 > numero_2 and numero_1 > numero_3):
+  print(f'{numero_1} é o maior')
+if(numero_2 > numero_1 and numero_2 > numero_3):
+  print(f'{numero_2} é o maior')
+if(numero_3 > numero_1 and numero_3 > numero_2):
+  print(f'{numero_3} é o maior')
