2f4d304391b6237803dd8d0ce3cfbc153cd1b31a
AugustoTI <augustocesar4001@gmail.com>
2023-12-05 13:14:19 -0300

    estrutura de repetição - for

---
+++ fundamentos/aula-14.py
@@ -0,0 +1,20 @@
+# Estruturas de repetição - For
+
+for i in range(0,6):
+  print(i)
+
+print('----------------------')
+
+for i in range(0,6,2):
+  print(i)
+
+print('----------------------')
+
+for i in range(6,0,-2):
+  print(i)
+
+print('----------------------')
+
+name = 'Augusto César'
+for letter in name:
+  print(letter)
